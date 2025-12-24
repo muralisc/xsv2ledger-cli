a593f6a02287a25d662e17c241cb8df0bc200d01
muralisc <muralisc@gmail.com>
2025-12-24 16:58:40 +0000

    Add should_exclude

--- src/xsv2ledger.rs
+++ src/xsv2ledger.rs
@@ -15,6 +15,47 @@ impl Xsv2Ledger {
             transactions_file_path: filepath,
         }
     }
+
+    fn should_exclude(&self, record: &csv::StringRecord) -> bool {
+        let mut should_exclude: bool = false;
+        for exclude_condition in &self.settings.exclude_conditions {
+            debug!("Checking Excluding condition: {:?}", exclude_condition);
+            match exclude_condition {
+                ExcludeCondition::ColumnContainsValue {
+                    column,
+                    value,
+                    operation,
+                } => {
+                    let column_under_check = &record[*column];
+                    if operation == "contains" {
+                        if column_under_check.contains(&*value.as_str()) {
+                            should_exclude = true;
+                        }
+                    } else if operation == "equal" {
+                        if column_under_check == *value {
+                            should_exclude = true;
+                        }
+                    }
+                }
+                ExcludeCondition::RecordLen(record_len) => {
+                    if *record_len == record.len() {
+                        should_exclude = true
+                    }
+                    debug!(
+                        "Excluding condition: {:?}, record len : {}, should_exclude: {}",
+                        record_len,
+                        record.len(),
+                        should_exclude
+                    );
+                }
+            }
+            if should_exclude == true  {
+                break;
+            }
+        }
+        return should_exclude;
+    }
+
     pub fn print(&self) {
         let mut reader = match csv::ReaderBuilder::new()
             .has_headers(self.settings.has_headers)
@@ -30,6 +71,11 @@ impl Xsv2Ledger {
             debug!("<========== Starting processsing of new record/row ============>");
             debug!("Record : {:?}!", record);
             debug!("Length of Row/Record {}", record.len());
+
+            if self.should_exclude(&record) {
+                info!("Excluding row: {:?}", record);
+                continue;
+            }
 
             let le = LedgerEntry::new(self.settings.clone(), record);
             le.print();
