0960473ec3c2900dc51c821fee24897acbd3fa2b
muralisc <muralisc@gmail.com>
2025-12-25 01:22:57 +0000

    Add Fidelity, Monzo

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 cargo run -- --transactions-csv test/test_xsv/fidelity-1999.csv --config config/fidelity.toml | diff test/expected/fidelity.ledger -
+
+cargo run -- --transactions-csv test/test_xsv/monzo.csv --config config/monzo.toml | diff test/expected/monzo.ledger -
