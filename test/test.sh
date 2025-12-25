e06088ef74d37d08dc71d5b19f17ac28ecbf0763
muralisc <muralisc@gmail.com>
2025-12-25 02:04:14 +0000

    Add Fidelity, Monzo, Schwab

--- test/test.sh
+++ test/test.sh
@@ -2,3 +2,7 @@
 cargo run -- --transactions-csv test/test_xsv/fidelity-1999.csv --config config/fidelity.toml | diff test/expected/fidelity.ledger -
 
 cargo run -- --transactions-csv test/test_xsv/monzo.csv --config config/monzo.toml | diff test/expected/monzo.ledger -
+
+
+cargo run -- --config config/schwab-trades.toml --transactions-csv test/test_xsv/schwab-trades.csv | diff test/expected/schwab-trades.ledger -
+
