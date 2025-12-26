5418c9257040d1f099fc1f82acd74820a2a5a3c8
muralisc <muralisc@gmail.com>
2025-12-26 08:26:32 +0000

    Update text files

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 cargo run -- --transactions-csv test/test_xsv/fidelity-1999.csv --config config/fidelity.toml | diff test/expected/fidelity.ledger -
+\ledger b --permissive -f test/expected/fidelity.ledger
 
 cargo run -- --transactions-csv test/test_xsv/monzo.csv --config config/monzo.toml| diff test/expected/monzo.ledger -
+\ledger b --permissive -f test/expected/monzo.ledger
 
 
 cargo run -- --config config/schwab-trades.toml --transactions-csv test/test_xsv/schwab-trades.csv| diff test/expected/schwab-trades.ledger -
+\ledger b --permissive -f test/expected/schwab-trades.ledger
 
 cargo run -- --config config/amex.toml --transactions-csv test/test_xsv/amex.csv| diff test/expected/amex.ledger -
+\ledger b --permissive -f test/expected/amex.ledger
 
 cargo run -- --config config/hsbc.toml --transactions-csv test/test_xsv/hsbc.csv| diff test/expected/hsbc.ledger -
+\ledger b --permissive -f test/expected/hsbc.ledger
